dbe9d1100c47b94608d339771c110fbe7ecc35ac
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2022-12-12 11:09:47 +0100

    Added Header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(){
+	return(
+		<header className="Header">
+			<div className="header-logo">
+				<img src="../images/troll-face.png" className="header-logo-image" />
+				<h2 className="header-logo-text">Meme Generator</h2>
+			</div>
+		</header>
+	)
+}
